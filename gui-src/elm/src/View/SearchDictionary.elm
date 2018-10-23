f5d46122d17c6266150edd5cab50f87782980533
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2018-10-23 19:11:51 +0100

    pagination and query options

--- gui-src/elm/src/View/SearchDictionary.elm
+++ gui-src/elm/src/View/SearchDictionary.elm
@@ -8,7 +8,7 @@ import Bulma.Layout as BL exposing (..)
 import Bulma.Modifiers exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick, onInput)
+import Html.Events exposing (onClick, onInput, onCheck)
 import Http
 import Json.Decode as Decode exposing (Decoder, int, list, string)
 import Json.Decode.Pipeline exposing (required)
@@ -24,6 +24,10 @@ cM =
 
 cSM =
     columnsModifiers
+
+
+mdRawHtml =
+    Just (MDConf.Options False MDConf.ParseUnsafe)
 
 
 view : (Msg m -> m) -> Model -> (Maybe (List (Html m)) -> Maybe (List (Html m)) -> Html m) -> List (Html m)
@@ -119,12 +123,15 @@ searchInput lift model =
             , onInput (\x -> lift (SetDictLookupQuery x))
             ]
     in
-    field []
-        [ controlLabel [] []
-        , controlText myControlInputModifiers
-            myControlAttrs
-            myInputAttrs
-            []
+    div []
+        [ field []
+              [ controlLabel [] []
+              , controlText myControlInputModifiers
+                  myControlAttrs
+                      myInputAttrs
+                      []
+              ]
+        , searchOptions lift model.queryOptions
         ]
 
 
@@ -159,7 +166,111 @@ viewLookupResults lift model =
             div [] [ text "Error: query failure" ]
 
         RemoteData.Success res ->
-            div [] (List.map (\x -> viewLookupResultRow x lift model) res)
+            viewQueryData lift res model
+
+
+viewQueryData : (Msg m -> m) -> DictSearches -> Model -> Html m
+viewQueryData lift data model =
+    let
+        hits =
+            "Results: " ++ String.fromInt data.total_count
+
+        all_words =
+            data.contains_exactly
+                |> List.append data.fulltext
+                |> List.append data.word_contains
+                |> List.append data.word_starts_with
+
+        p =
+            model.paginationInfo
+
+        t =
+            List.drop ((p.currentPage - 1) * p.pageLength) all_words
+
+        show_words =
+            List.take p.pageLength t
+    in
+    div []
+        [ div [ style "padding-bottom" "2em" ] [ text hits ]
+        , wordPages lift model.paginationInfo
+        , div [] (List.map (\x -> viewLookupResultRow lift x model) show_words)
+        ]
+
+
+wordPages : (Msg m -> m) -> PaginationInfo -> Html m
+wordPages lift paginationInfo =
+    let
+        pInf =
+            paginationInfo
+
+        cP =
+            pInf.currentPage
+
+        tP =
+            pInf.totalPages
+
+        pageList =
+            if pInf.totalPages <= 10 then
+                List.map
+                    (\x ->
+                        paginationLink (x == cP)
+                            [ onClick (lift (SetPagination x)) ]
+                            [ text (String.fromInt x) ]
+                    )
+                    (List.range 1 tP)
+
+            else if cP <= 3 then
+                [ paginationLink (1 == cP) [ onClick (lift (SetPagination 1)) ] [ text "1" ]
+                , paginationLink (2 == cP) [ onClick (lift (SetPagination 2)) ] [ text "2" ]
+                , paginationLink (3 == cP) [ onClick (lift (SetPagination 3)) ] [ text "3" ]
+                , paginationEllipsis [] [ text "..." ]
+                , paginationLink False
+                    [ onClick (lift (SetPagination tP)) ]
+                    [ text (String.fromInt tP) ]
+                ]
+
+            else if cP > 3 && cP < tP - 2 then
+                [ paginationLink False
+                    [ onClick (lift (SetPagination 1)) ]
+                    [ text "1" ]
+                , paginationEllipsis [] [ text "..." ]
+                , paginationLink False
+                    [ onClick (lift (SetPagination (cP - 1))) ]
+                    [ text (String.fromInt (cP - 1)) ]
+                , paginationLink True
+                    []
+                    [ text (String.fromInt cP) ]
+                , paginationLink False
+                    [ onClick (lift (SetPagination (cP + 1))) ]
+                    [ text (String.fromInt (cP + 1)) ]
+                , paginationEllipsis [] [ text "..." ]
+                , paginationLink False
+                    [ onClick (lift (SetPagination tP)) ]
+                    [ text (String.fromInt tP) ]
+                ]
+
+            else
+                [ paginationLink False
+                    [ onClick (lift (SetPagination 1)) ]
+                    [ text "1" ]
+                , paginationEllipsis [] [ text "..." ]
+                , paginationLink (tP - 2 == cP)
+                    [ onClick (lift (SetPagination (tP - 2))) ]
+                    [ text (String.fromInt (tP - 2)) ]
+                , paginationLink (tP - 1 == cP)
+                    [ onClick (lift (SetPagination (tP - 1))) ]
+                    [ text (String.fromInt (tP - 1)) ]
+                , paginationLink (tP == cP)
+                    [ onClick (lift (SetPagination tP)) ]
+                    [ text (String.fromInt tP) ]
+                ]
+    in
+    pagination Left
+        []
+        [ paginationPrev [ onClick (lift PaginationPrev) ] [ text "Previous" ]
+        , paginationNext [ onClick (lift PaginationNext) ] [ text "Next" ]
+        , paginationList [] pageList
+        ]
 
 
 
@@ -167,8 +278,8 @@ viewLookupResults lift model =
 -- TODO: for fulltext matches, show the definition_plain snippet
 
 
-viewLookupResultRow : DictWord -> (Msg m -> m) -> Model -> Html m
-viewLookupResultRow dictWord lift model =
+viewLookupResultRow : (Msg m -> m) -> DictWord -> Model -> Html m
+viewLookupResultRow lift dictWord model =
     let
         s =
             if String.length dictWord.summary > 0 then
@@ -264,6 +375,41 @@ viewSelectedResultRow dictWord lift model =
         ]
 
 
+searchOptions : (Msg m -> m) -> QueryOptions -> Html m
+searchOptions lift opts =
+    fields Left
+        []
+        [ controlCheckBox False
+            []
+            []
+            [ onCheck (\x -> lift (SetOptWordStartsWith x))
+            , checked opts.wordStartsWith
+            ]
+            [ text "word starts with" ]
+        , controlCheckBox False
+            []
+            []
+            [ onCheck (\x -> lift (SetOptWordContains x))
+            , checked opts.wordContains
+            ]
+            [ text "word contains" ]
+        , controlRadio []
+            [ controlRadioButton False
+                opts.fulltext
+                "fulltext"
+                []
+                [ onCheck (\x -> lift (SetOptFulltext x)) ]
+                [ text "fulltext matching" ]
+            , controlRadioButton False
+                opts.containsExactly
+                "exactly"
+                []
+                [ onCheck (\x -> lift (SetOptContainsExactly x)) ]
+                [ text "exactly matching" ]
+            ]
+        ]
+
+
 myColumnsModifiers : ColumnsModifiers
 myColumnsModifiers =
     { multiline = False
@@ -275,9 +421,11 @@ myColumnsModifiers =
 
 type alias Model =
     { lookupQuery : String
-    , lookupResults : WebData (List DictWord)
+    , lookupResults : WebData DictSearches
     , selectedWordsList : List DictWord
+    , paginationInfo : PaginationInfo
     , subRoute : SubRoute
+    , queryOptions : QueryOptions
     }
 
 
@@ -285,7 +433,41 @@ initialModel =
     { lookupQuery = ""
     , lookupResults = RemoteData.NotAsked
     , selectedWordsList = []
+    , paginationInfo = initialPaginationInfo
     , subRoute = Searching
+    , queryOptions = initialQueryOptions
+    }
+
+
+initialPaginationInfo : PaginationInfo
+initialPaginationInfo =
+    { pageLength = 30
+    , currentPage = 1
+    , totalPages = 1
+    }
+
+
+initialQueryOptions : QueryOptions
+initialQueryOptions =
+    { wordStartsWith = True
+    , wordContains = True
+    , fulltext = True
+    , containsExactly = False
+    }
+
+
+type alias QueryOptions =
+    { wordStartsWith : Bool
+    , wordContains : Bool
+    , fulltext : Bool
+    , containsExactly : Bool
+    }
+
+
+type alias PaginationInfo =
+    { pageLength : Int
+    , currentPage : Int
+    , totalPages : Int
     }
 
 
@@ -302,6 +484,15 @@ type alias DictWord =
     }
 
 
+type alias DictSearches =
+    { word_starts_with : List DictWord
+    , word_contains : List DictWord
+    , contains_exactly : List DictWord
+    , fulltext : List DictWord
+    , total_count : Int
+    }
+
+
 type SubRoute
     = Searching
     | Reading
@@ -310,11 +501,26 @@ type SubRoute
 type Msg m
     = NoOp
     | SetDictLookupQuery String
-    | DictLookupDataReceived (WebData (List DictWord))
+    | DictLookupDataReceived (WebData DictSearches)
     | AddToSelectedResults DictWord
     | RemoveFromSelectedResults DictWord
     | AddToDictInput String
     | SetSubRoute SubRoute
+    | PaginationPrev
+    | PaginationNext
+    | SetPagination Int
+    | SetOptWordStartsWith Bool
+    | SetOptWordContains Bool
+    | SetOptFulltext Bool
+    | SetOptContainsExactly Bool
+
+
+lookupCmd lift query opts =
+    if String.length query > 2 then
+        fetchDictWord lift query opts
+
+    else
+        Cmd.none
 
 
 update : (Msg m -> m) -> Msg m -> Model -> ( Model, Cmd m )
@@ -324,18 +530,41 @@ update lift msg model =
             ( model, Cmd.none )
 
         SetDictLookupQuery query ->
-            let
-                lookupCmd =
-                    if String.length query > 2 then
-                        fetchDictWord lift query
-
-                    else
-                        Cmd.none
-            in
-            ( { model | lookupQuery = query }, lookupCmd )
+            ( { model | lookupQuery = query }, lookupCmd lift query model.queryOptions )
 
         DictLookupDataReceived data ->
-            ( { model | lookupResults = data }, Cmd.none )
+            case data of
+                RemoteData.NotAsked ->
+                    ( { model | lookupResults = data }, Cmd.none )
+
+                RemoteData.Loading ->
+                    ( { model | lookupResults = data }, Cmd.none )
+
+                RemoteData.Failure _ ->
+                    ( { model | lookupResults = data }, Cmd.none )
+
+                RemoteData.Success res ->
+                    let
+                        p =
+                            model.paginationInfo
+
+                        count =
+                            res.total_count
+
+                        t =
+                            ceiling (toFloat count / toFloat p.pageLength)
+
+                        curr =
+                            if p.currentPage > t then
+                                t
+
+                            else
+                                p.currentPage
+
+                        p_ =
+                            { p | totalPages = t, currentPage = curr }
+                    in
+                    ( { model | lookupResults = data, paginationInfo = p_ }, Cmd.none )
 
         AddToSelectedResults dictWord ->
             let
@@ -366,10 +595,94 @@ update lift msg model =
                 q =
                     model.lookupQuery ++ letter
             in
-            ( { model | lookupQuery = q }, fetchDictWord lift q )
+            ( { model | lookupQuery = q }, fetchDictWord lift q model.queryOptions )
 
         SetSubRoute x ->
             ( { model | subRoute = x }, Cmd.none )
+
+        PaginationPrev ->
+            let
+                p =
+                    model.paginationInfo
+
+                curr =
+                    if p.currentPage > 1 then
+                        p.currentPage - 1
+
+                    else
+                        p.currentPage
+
+                p_ =
+                    { p | currentPage = curr }
+            in
+            ( { model | paginationInfo = p_ }, Cmd.none )
+
+        PaginationNext ->
+            let
+                p =
+                    model.paginationInfo
+
+                curr =
+                    if p.currentPage < p.totalPages then
+                        p.currentPage + 1
+
+                    else
+                        p.currentPage
+
+                p_ =
+                    { p | currentPage = curr }
+            in
+            ( { model | paginationInfo = p_ }, Cmd.none )
+
+        SetPagination x ->
+            let
+                p =
+                    model.paginationInfo
+
+                p_ =
+                    { p | currentPage = x }
+            in
+            ( { model | paginationInfo = p_ }, Cmd.none )
+
+        SetOptWordStartsWith x ->
+            let
+                o =
+                    model.queryOptions
+
+                o_ =
+                    { o | wordStartsWith = x }
+            in
+            ( { model | queryOptions = o_ }, lookupCmd lift model.lookupQuery o_ )
+
+        SetOptWordContains x ->
+            let
+                o =
+                    model.queryOptions
+
+                o_ =
+                    { o | wordContains = x }
+            in
+            ( { model | queryOptions = o_ }, lookupCmd lift model.lookupQuery o_ )
+
+        SetOptFulltext x ->
+            let
+                o =
+                    model.queryOptions
+
+                o_ =
+                    { o | fulltext = x, containsExactly = not x }
+            in
+            ( { model | queryOptions = o_ }, lookupCmd lift model.lookupQuery o_ )
+
+        SetOptContainsExactly x ->
+            let
+                o =
+                    model.queryOptions
+
+                o_ =
+                    { o | fulltext = not x, containsExactly = x }
+            in
+            ( { model | queryOptions = o_ }, lookupCmd lift model.lookupQuery o_ )
 
 
 dictWordDecoder : Decoder DictWord
@@ -386,13 +699,44 @@ dictWordDecoder =
         |> required "to_lang" string
 
 
-fetchDictWord : (Msg m -> m) -> String -> Cmd m
-fetchDictWord lift query =
-    list dictWordDecoder
-        |> Http.get (UB.absolute [ "search", "dict_words" ] [ UB.string "query" query ])
+dictSearchesDecoder : Decoder DictSearches
+dictSearchesDecoder =
+    Decode.succeed DictSearches
+        |> required "word_starts_with" (list dictWordDecoder)
+        |> required "word_contains" (list dictWordDecoder)
+        |> required "contains_exactly" (list dictWordDecoder)
+        |> required "fulltext" (list dictWordDecoder)
+        |> required "total_count" int
+
+
+optsToQuery opts =
+    let
+        f =
+            \x ->
+                if Tuple.second x then
+                    UB.string (Tuple.first x) "true"
+
+                else
+                    UB.string (Tuple.first x) "false"
+    in
+    List.map f
+        [ ( "word_starts_with", opts.wordStartsWith )
+        , ( "word_contains", opts.wordContains )
+        , ( "fulltext", opts.fulltext )
+        , ( "contains_exactly", opts.containsExactly )
+        ]
+
+
+fetchDictWord : (Msg m -> m) -> String -> QueryOptions -> Cmd m
+fetchDictWord lift query opts =
+    dictSearchesDecoder
+        |> Http.get
+            (UB.absolute
+                [ "search", "dict_words" ]
+                (List.append
+                    [ UB.string "query" query ]
+                    (optsToQuery opts)
+                )
+            )
         |> RemoteData.sendRequest
         |> Cmd.map (\x -> lift (DictLookupDataReceived x))
-
-
-mdRawHtml =
-    Just (MDConf.Options False MDConf.ParseUnsafe)
